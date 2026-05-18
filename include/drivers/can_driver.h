a7142de21478bf1ee452181b3eb1e2539b5724a5
bruvv <3063928+bruvv@users.noreply.github.com>
2026-05-18 13:27:18 +0200

    Add CAN driver diagnostics hooks

--- include/drivers/can_driver.h
+++ include/drivers/can_driver.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "../can_frame_types.h"
+#include <stddef.h>
+#include <stdio.h>
 
 struct CanDriver
 {
@@ -11,5 +13,20 @@ struct CanDriver
     virtual bool enableInterrupt(void (*onReady)()) = 0;
     virtual bool read(CanFrame &frame) = 0;
     virtual bool send(const CanFrame &frame) = 0;
+
+    virtual void diagnosticsJson(char *out, size_t outLen) const
+    {
+        if (!out || outLen == 0)
+            return;
+        snprintf(out, outLen, "{\"type\":\"generic\"}");
+    }
+
+    virtual void diagnosticsSummary(char *out, size_t outLen) const
+    {
+        if (!out || outLen == 0)
+            return;
+        snprintf(out, outLen, "CAN driver diagnostics unavailable");
+    }
+
     virtual ~CanDriver() = default;
 };
